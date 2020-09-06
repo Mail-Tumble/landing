b71a2c1eea4520a132f01b21a6f896b5f601536f
Alex Shelkovskiy <alexshelkov@gmail.com>
2020-09-07 03:48:03 +0600

    Pages

---
+++ precache-manifest.bab7c4e6ff69ac123e418fffbe8eea12.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525423bdfce355da1c1cdde094da348d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6cfc35f5fc3173d93d8",
+    "url": "/static/css/2.5d8f65bd.chunk.css"
+  },
+  {
+    "revision": "aba00d322e8651db758a",
+    "url": "/static/css/main.569573f5.chunk.css"
+  },
+  {
+    "revision": "b6cfc35f5fc3173d93d8",
+    "url": "/static/js/2.f1c0db5f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.f1c0db5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba00d322e8651db758a",
+    "url": "/static/js/main.65f27912.chunk.js"
+  },
+  {
+    "revision": "6a06dd8c58e6064993b5",
+    "url": "/static/js/runtime-main.83421ab0.js"
+  },
+  {
+    "revision": "0d60f46bb32f16bf1b7d629f6ffda952",
+    "url": "/static/media/1.0d60f46b.png"
+  },
+  {
+    "revision": "0167441ace61cf4216b3f7e2d107f94c",
+    "url": "/static/media/2.0167441a.png"
+  },
+  {
+    "revision": "73160335b87ffd37f185a3b9e3b96a93",
+    "url": "/static/media/bg1.73160335.jpg"
+  },
+  {
+    "revision": "2e19c22e788d3d5dd2e35588f69b4c5b",
+    "url": "/static/media/bg2.2e19c22e.jpg"
+  },
+  {
+    "revision": "e5ac82a258a1c1a4d62b7bacae7195c8",
+    "url": "/static/media/bg3.e5ac82a2.jpg"
+  },
+  {
+    "revision": "cd2a55b682f6476b2db8b71a95c94b0a",
+    "url": "/static/media/bg4.cd2a55b6.jpg"
+  },
+  {
+    "revision": "4321a15d0a8a36a89f7886292ad2de80",
+    "url": "/static/media/bg5.4321a15d.jpg"
+  },
+  {
+    "revision": "5899abd89c9c351bebfd35bd5ee371d4",
+    "url": "/static/media/coles.5899abd8.png"
+  },
+  {
+    "revision": "a0d7985934f4fa2365ff3483a33eff97",
+    "url": "/static/media/f45-training.a0d79859.png"
+  },
+  {
+    "revision": "d3e92ea8122ac428f7dfc0be3720a451",
+    "url": "/static/media/land-rover.d3e92ea8.png"
+  },
+  {
+    "revision": "97714f0db9ff1525d9c9d299e28e737b",
+    "url": "/static/media/luna-park.97714f0d.png"
+  },
+  {
+    "revision": "674adafe5c40e6ce0937be8e23b4b589",
+    "url": "/static/media/particles-2.674adafe.svg"
+  },
+  {
+    "revision": "ad45b6128c5e3564a994a4edf7448734",
+    "url": "/static/media/people-1.ad45b612.jpg"
+  },
+  {
+    "revision": "09c973c517d02e819801ebddeb2e8c10",
+    "url": "/static/media/people-2.09c973c5.jpg"
+  },
+  {
+    "revision": "e378452fdab1e101ce93d07079f22a2a",
+    "url": "/static/media/people-3.e378452f.jpg"
+  },
+  {
+    "revision": "d8d722379ae7ec816cbb442853efa64d",
+    "url": "/static/media/subway.d8d72237.png"
+  }
+]);
--- src/layout-blueprints/index.js
+++
@@ -1,1 +0,0 @@
-export { default as PresentationLayout } from './PresentationLayout';
